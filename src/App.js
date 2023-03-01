ba1f0fcfaccb2f0372931e6a8b0cbd1d5519da89
dumulnet@gmail.com <dumulnet@gmail.com>
2023-03-01 14:21:06 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
+// import useEffect hook
+import React, { useEffect, useState }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+import { Auth } from 'aws-amplify'
+
+function checkUser() {
+   Auth.currentAuthenticatedUser()
+    .then(user => console.log({ user }))
+    .catch(err => console.log(err));
+}
+
+function signOut() {
+  Auth.signOut()
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
+
+function App(props) {
+   const [user, setUser] = useState('')
+   useEffect(() => {
+
+   Auth.currentAuthenticatedUser()
+    .then(user => {
+		console.log({ user });
+                setUser('bob');
+	})
+    .catch(err => console.log(err));
+   }, []);
+  
+
+   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={checkUser}>Check User</button>
+        {user === '' ? (
+		<button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+                ) : (
+		<button onClick={signOut}>Sign Out</button>
+                )}
+
       </header>
     </div>
   );
+
+
 }
 
-export default App;
+
+export default App
+
